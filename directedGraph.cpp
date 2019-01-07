e0237674519e9d630e16c5af59808269c13e9e20
Manish Sharma <31279522+mnsgeeks@users.noreply.github.com>
2019-01-07 17:59:32 +0530

    Directed Graph in c++

---
+++ directedGraph.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+#include<vector>
+using namespace std;
+
+
+void findOutdegree(int source, vector< pair<int,int > > &edge)
+{
+ int count=0;
+ for(int i=0;i<6;i++)
+ {
+   if(edge[i].first == source)
+   { count++; }
+
+ }
+cout<<"\nOutDegree of this Source Vertex "<<count<<endl;
+
+}
+
+
+
+int main()
+{
+ vector< pair<int,int> > edge;
+
+cout<<" Enter Paths "<<endl;
+   //int arr[] = {10, 20, 5, 40 }; 
+    //int arr1[] = {30, 60, 20, 50}; 
+
+for(int i=0;i<6;i++)
+{
+ int a,b;
+ cin>>a>>b;
+ 
+ edge.push_back(make_pair(a,b));
+
+}
+
+cout<<" All Paths "<<endl;
+
+for(int i=0;i<6;i++)
+{
+  cout<<edge[i].first<<" "<<edge[i].second<<endl;
+}
+
+findOutdegree(3,edge);  // function to find Ot=utdegree of any vertex
+
+}
+
+
+
