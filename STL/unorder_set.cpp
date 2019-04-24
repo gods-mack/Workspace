1b1476fee2d3dad403c3cefbd57318688f29059e
Manish Sharma <31279522+gods-mack@users.noreply.github.com>
2019-04-25 00:52:29 +0530

    Hash or Unorder_set in c++ STL

---
+++ STL/unorder_set.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+unordered_set<string> a;
+
+a.insert("Manish");
+a.insert("Kathy");
+a.insert("John");
+a.insert("kim");
+a.insert("Tony");
+a.insert("Ravi");
+
+string key="kim";
+
+if(a.find(key)==a.end())
+{  cout<<"Not Found "; c}
+else
+{ cout<<"Found " ; }
+
+}
+
+
+
