bf7041a86ad9fa852b3909d59e5b53665391f3dc
Manish Sharma <31279522+gods-mack@users.noreply.github.com>
2019-06-06 00:10:50 +0530

    Calculate a raised to the power of b, using recursion

---
+++ Recursion-practice/a^b.cpp
@@ -0,0 +1,31 @@
+// Calculate a raised to the power of b , using Recursion
+
+#include<iostream>
+#include<cstring>
+#include<cstdlib>// 
+
+using namespace std;
+
+
+int power(int a,int b){
+	 
+	if(b==0){
+		return 1;
+	}
+	
+	else
+		{
+			
+		return  a*power(a,b-1);
+		
+	}
+}
+
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+	
+	cout<<power(a,b);
+}
+
