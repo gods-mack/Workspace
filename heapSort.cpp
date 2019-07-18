3c19788874ef1973d9c112bb6ac84e914bf36268
Manish Sharma <31279522+gods-mack@users.noreply.github.com>
2019-07-18 22:04:57 +0530

    HeapSort

--- heapSort.cpp
+++ heapSort.cpp
@@ -3,35 +3,37 @@ using namespace std;
 
 void heapfy(int a[],int n,int i)
 {
-int smallest=i;
- int l=2*i+1;
- int r=2*i+2;
+        int smallest=i;
+        int l=2*i+1;
+        int r=2*i+2;
  
- if(l<n  && a[l]<a[smallest])
- { smallest=l;
- }
- if(r<n && a[r]<a[smallest])
- { smallest=r;
-  }
+        if(l<n  && a[l]<a[smallest])
+        {
+            smallest=l;
+        }
+        if(r<n && a[r]<a[smallest])
+        { 
+            smallest=r;
+         }
 
 
- if(smallest!=i)
- { swap(a[i],a[smallest]);
- 
- 
- heapfy(a,n,smallest);
- }
+       if(smallest!=i)
+       { 
+           swap(a[i],a[smallest]);
+           heapfy(a,n,smallest);
+        }
 }
 
 void heapmin(int a[],int n)
 {
-  for(int i=(n/2)-1;i>=0;i--)
-  { 
-   heapfy(a,n,i);
-  }
-  for(int i=n-1;i>=0;i--)
- {  swap(a[0],a[i]);
-  heapfy(a,i,0); }
+    for(int i=(n/2)-1;i>=0;i--)
+    { 
+        heapfy(a,n,i);
+    }
+    for(int i=n-1;i>=0;i--)
+    {   swap(a[0],a[i]);
+        heapfy(a,i,0); 
+    }
 
 }
 
@@ -39,13 +41,16 @@ void heapmin(int a[],int n)
 
 
 int main()
-{ int n; cin>>n;int a[n];
-  for(int i=0;i<n;i++)
-  { cin>>a[i]; } 
-  heapmin(a,n);
- cout<<"After "<<endl;
-  for(int j=0;j<n;j++)
- { cout<<a[j]<<" "; }
+{   
+    int n; cin>>n;int a[n];
+    for(int i=0;i<n;i++)
+        { cin>>a[i]; } 
+    heapmin(a,n);
+    cout<<"After Sorting"<<endl;
+    for(int j=0;j<n;j++)
+    { 
+        cout<<a[j]<<" "; 
+    }
 
 }
 
