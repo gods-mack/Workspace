10ac14322eb9a065c7450b7fe0497ead62946bd1
Manish <sehorem@gmail.com>
2019-12-06 12:02:53 +0530

    two string match

---
+++ OS/prac3.sh
@@ -0,0 +1,8 @@
+read -p "Enter first string: " str1
+read -p "Enter second string: " str2
+if [ "$str1" == "$str2" ]
+then
+  echo "Both strings are same"
+else
+  echo "Both strings are different"
+fi
